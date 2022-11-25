83774d677088797a3369f74e7df1fc24215f9a7f
Hind Chenfour <hind.chenfour123@gmail.com>
2022-11-25 18:01:30 +0000

    Creating data access object classes for MySQL database

---
+++ formulaire/dao/MySqlDatabase.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MySql.Data.MySqlClient;
+using System.Data.OleDb;
+using System.Data;
+using System.IO;
+
+namespace GestionExam.dao {
+    class MySqlDatabase {
+        private String server = "localhost";
+        private String user;
+        private String pwd;
+        private String source;
+
+        private String connectionString;
+        private MySqlConnection db;
+
+
+        public MySqlDatabase() {
+
+        }
+
+        public MySqlDatabase(String user, String pwd, String source) {
+            this.user = user;
+            this.pwd = pwd;
+            this.source = source;
+
+            connectionString = "SERVER=" + server + ";DATABASE=" + source + ";UID=" + user + ";PASSWORD=" + pwd;
+
+            OpenConnection();
+        }
+
+        public MySqlDatabase(String source) : this("root", "", source) {
+
+        }
+
+        public MySqlDatabase(String server, String user, String pwd, String source) {
+            this.server = server;
+            this.user = user;
+            this.pwd = pwd;
+            this.source = source;
+
+            connectionString = "SERVER=" + server + ";DATABASE=" + source + ";UID=" + user + ";PASSWORD=" + pwd;
+            OpenConnection();
+        }
+
+        public void OpenConnection() {
+            db = new MySqlConnection(connectionString);
+            try {
+                db.Open();
+                Console.WriteLine("Connection établie ...");
+            }
+
+            catch (Exception e) {
+                Console.WriteLine("Erreur : " + e.Message);
+            }
+        }
+
+        public List<Object[]> SelectQuery(String query) {
+            MySqlCommand cmd = new MySqlCommand(query, db);
+            List<Object[]> data = new List<Object[]>();
+
+            try {
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                int cols = reader.FieldCount;
+
+                while (reader.Read()){
+                    Object[] row = new Object[cols];
+
+                    for (int i = 0; i < cols; i++) {
+                        row[i] = reader.GetValue(i);
+                    }
+
+                    data.Add(row);
+                }
+                reader.Close();
+                return data;
+            }
+
+            catch (Exception e) {
+                Console.WriteLine("Erreur : " + e.Message);
+                 return null;
+           }
+        }
+
+        public int UpdateQuery(String query) {
+            MySqlCommand cmd = new MySqlCommand(query, db);
+
+            try {
+                return cmd.ExecuteNonQuery();
+            }
+
+            catch (Exception e) {
+                Console.WriteLine("Erreur : " + e.Message);
+                return -1;
+            }
+        }
+
+        public int insert(String tableName, params Object[] row) {
+            String query = "INSERT INTO " + tableName + " VALUES('" + row[0] + "'";
+
+            for (int i = 1; i < row.Length; i++) {
+                query += ", '" + row[i] + "'";
+            }
+            query += ")";
+
+            return UpdateQuery(query);
+        }
+
+        public List<Object[]> select(String tableName, String columnName, Object value) {
+            String query = "SELECT * FROM " + tableName + " WHERE " + columnName + " = '" + value + "'";
+            return SelectQuery(query);
+        }
+
+        public List<Object[]> selectAll(String tableName) {
+            String query = "SELECT * FROM " + tableName;
+            return SelectQuery(query);
+        }
+     }
+}
---
+++ formulaire/dao/ProfesseurDao.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GestionExam.dao;
+using GestionExam.models;
+
+namespace GestionExam.dao {
+    class ProfesseurDao {
+        private MySqlDatabase db;
+        private const String TABLE_NAME = "professeur";
+
+        public ProfesseurDao(MySqlDatabase db) {
+            this.db = db;
+        }
+
+        public Professeur selectById(String id) {
+            Object[] element = db.select(TABLE_NAME, "cni", id).ElementAt(0);
+
+            return new Professeur(element[0].ToString(), element[1].ToString(), element[2].ToString(), element[3].ToString());
+        }
+
+        public List<Professeur> selectAll() {
+            List<Object[]> data = db.selectAll(TABLE_NAME);
+            List<Professeur> elements = new List<Professeur>();
+
+            for (int i = 0; i < data.Count; i++) {
+                Object[] element = data.ElementAt(i);
+                elements.Add(new Professeur(element[0].ToString(), element[1].ToString(), element[2].ToString(), element[3].ToString()));
+            }
+            return elements;
+        }
+
+
+        public void insert(Professeur p) {
+            db.insert(TABLE_NAME, p.GetCni(), p.GetNom(), p.GetPrenom(), p.GetTel());
+        }
+    }
+}
